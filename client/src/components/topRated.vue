5083a951f61869eec0f226d4a6733c6096cdef70
pkurjanowicz5 <pkurjanowicz5@gmail.com>
2019-12-07 10:20:27 -0700

    fixed issue

---
+++ client/src/components/topRated.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <h1>Top Liked Poops</h1>
+        <span v-for='(poop, index) in poops' :key='index'>{{poop}}</span>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'topRated',
+    props: ['poops'],
+    // data() {
+    //     return {
+    //         poops: '',
+    //     }
+    // }
+}
+</script>
